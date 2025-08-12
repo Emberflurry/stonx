c1291e84090752ddaf8dcd601bdad35e1febb167
John DeForest <juandelbosque373@gmail.com>
2025-08-12 14:35:47 -0400

    filling missings, new bigdata scripts
    lot of manual pulling to do, yeowsch. frick ticker changes that shi annoying asl. some cleanup of old files too

--- investpy_puller.py
+++
@@ -1,9 +0,0 @@
-#FAILED - blocked API call 403
-from investiny import search_assets
-
-res = search_assets(query='CSTR',limit=1,
-                    type='Stock')
-#fullname= 'Fomo Worldwide, Inc.'
-print(res)
-
-#"CSTR", "01/01/2023", "31/05/2023"
